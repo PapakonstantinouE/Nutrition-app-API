2b7adc5a77093dab85d06b960d6f34d3e13ed037
olyxen <dai16058@uom.edu.gr>
2020-02-01 02:10:03 +0200

    Try to make backend work on Heroku

--- server.js
+++ server.js
@@ -1,14 +1,34 @@
 const express = require('express')
 var cors = require('cors');
-
+const mysql = require('mysql');
+const bodyParser = require('body-parser');
 
 var app = express();
-var port = 8000;
+var port = process.env.PORT || 8000;
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
+
+const pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+});
+
 app.get('/hello', (req, res) => res.send('Hello World!'))
-app.get('/hellooo', (req, res) => res.send('Hello World!'))
+
+app.get('/',(req,res) => {
+    res.send('*ROOT ENDPOINT* API is up & working.');
+});
+
+app.get('/api',(req,res) => {
+    res.send('API is up & working.');
+});
 
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port,() => console.log(`Listening on port ${port}`)); 
+
