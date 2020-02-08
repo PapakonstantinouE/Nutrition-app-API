36cdef9d4ebf0259fcc42da82c934098cf12955b
olyxen <dai16058@uom.edu.gr>
2020-02-08 20:25:02 +0200

    Connect the Database with the server

--- server.js
+++ server.js
@@ -1,22 +1,29 @@
-const express = require('express')
+const express = require('express');
 var cors = require('cors');
 const mysql = require('mysql');
 const bodyParser = require('body-parser');
 
+const dotenv = require('dotenv');
+dotenv.config();
+
 var app = express();
-var port = process.env.PORT || 8000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
 
-const pool = mysql.createPool({
+var con = mysql.createConnection({
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME
+});
+
+con.connect(function(err){
+    if(err) throw err;
+    console.log("Connected to DB");
 });
 
 app.get('/hello', (req, res) => res.send('Hello World!'))
@@ -29,6 +36,6 @@ app.get('/api',(req,res) => {
     res.send('API is up & working.');
 });
 
-
+var port = process.env.PORT || 8080;
 app.listen(port,() => console.log(`Listening on port ${port}`)); 
 
