df61868a9c6ba721d8aa8c501778f1aa6a43eac4
olyxen <dai16058@uom.edu.gr>
2020-02-10 00:05:28 +0200

    commit to test heroku

--- server.js
+++ server.js
@@ -35,4 +35,46 @@ app.listen(port,() => console.log(`Listening on port ${port}`));
 
 // NECESSARY TO VALIDATE FATSECRET TOKEN
 //the request is big so i transfered it in fatsecret.js and i call it from there
-app.get('/api/tokenTest', fatsecret.getToken);
+// app.get('/api/tokenTest', fatsecret.getToken);
+
+//NECESSARY TO VALIDATE FATSECRET TOKEN
+clientID = process.env.CLIENT_ID,
+clientSecret = process.env.CLIENT_SECRET
+
+var options = {
+   method: 'POST',
+   url: 'https://oauth.fatsecret.com/connect/token',
+   method : 'POST',
+   auth : {
+      user : clientID,
+      password : clientSecret
+   },
+   headers: { 'content-type': 'application/json'},
+   form: {
+      'grant_type': 'client_credentials',
+      'scope' : 'basic'
+   },
+   json: true
+};
+app.get('/api/tokenTest', (req, res) =>{
+    try {
+        var current_time = (new Date() / 1000);
+        pool.query("SELECT * FROM fatsecret", function (err, result) {
+            const data_time = result[0].time;
+            if(current_time<data_time){
+                //do nothing
+                res.send("Token is working fine")
+            }else{
+                request(options, function (error, response, body) {
+                    if (error) throw new Error(error); 
+                    const token = body.access_token;
+                    pool.query(`UPDATE fatsecret SET time = ${current_time}, token = '${token}' WHERE id = 1`, function (err){
+                        res.send("Token successfully updated")
+                    })
+                });
+            }
+        });
+    } catch (err) {
+        console.log(err);
+    }
+})
