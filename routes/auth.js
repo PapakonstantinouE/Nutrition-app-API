778fd5baa4d02f9e3007f124abd1bff40123568a
olyxen <dai16058@uom.edu.gr>
2020-03-31 21:53:04 +0300

    Make auth work for mongoDB and copy the previous version to authmysql

--- routes/auth.js
+++ routes/auth.js
@@ -1,100 +1,130 @@
 const router = require('express').Router();
-const mysql = require('mysql');
-require('dotenv').config();
+const User = require ('../model/user');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const request = require("request");
 const {registerValidation,loginValidation} = require('../validation');
 const verify = require('./verifyToken');
 
-var pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
-});
 
-
-router.post('/register', (req,res) => {
+router.post('/validateSignup', async (req,res) => {
     // VALIDATE THE DATA BEFORE MAKE A USER
     const {error} = registerValidation(req.body);
-    if (error) return res.status(400).send(error.details[0].message);
+    if (error) return res.send(error.details[0].message);
 
+    //CHECKING IF EMAIL ALREADY EXISTS
+    const emailExist = await User.findOne({email: req.body.email})
+    if (emailExist) return res.send("Email already exists");
+
+    //CHECKING IF USERNAME ALREADY EXISTS
+    const usernameExist = await User.findOne({username: req.body.username})
+    if (usernameExist) return res.send("This username is already used, please try another one.");
+
+    res.send("It's ok");
+})
+
+
+
+router.post('/register', async (req, res) => {
+    
+    // VALIDATE THE DATA BEFORE MAKE A USER
+    const {error} = registerValidation(req.body);
+    if (error) return res.send(error.details[0].message);
+
+    //CHECKING IF USER ALREADY EXISTS
+    const emailExist = await User.findOne({email: req.body.email})
+    if (emailExist) return res.send("Email already exists");
+
+    //CHECKING IF USERNAME ALREADY EXISTS
+    const usernameExist = await User.findOne({username: req.body.username})
+    if (usernameExist) return res.send("This username is already used, please try another one.");
+ 
+    //HASH PASSWORDS
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+    const height = req.body.height;
+    const weight = req.body.weight;
+    const age = req.body.age;
+    const gender = req.body.gender;
+    
+    //CREATE A NEW USER
+    const user = new User({
+        username: req.body.username,
+        email: req.body.email,
+        password: hashedPassword,
+        height: height,
+        weight: weight,
+        age: age,
+        gender: gender
+    });
     try{
-        var height = req.body.height;
-        var weight = req.body.weight;
-        var age = req.body.age;
-        var gender = req.body.gender;
-
-        // var checkValues = [ req.body.email, req.body.username]
-        var checkValues = [req.body.username]
-        var tempValues  = [ req.body.email, req.body.username, req.body.password, req.body.weight, req.body.height, req.body.age, req.body.gender]
-
-        // pool.query(`SELECT * FROM users WHERE email=? or username=?`, checkValues, function(err,result1){
-        pool.query(`SELECT * FROM users WHERE username=?`, checkValues, function(err,result1){
-            if (!isEmptyObject(result1)) return res.status(400).send("Username or email already exist, try another one"); 
-            else{
-                //First find bmr
-                var options = {
-                    method: 'GET',
-                    url: `https://urvipaithankar.herokuapp.com/bmr/index.php/${height}/${weight}/${age}/${gender} 
-                })}`
-                };
-                request(options, function (error, response, body) {
-                    if (error) throw new Error(error); 
-                    else{
-                        //and when you find it, then create the user with all the details
-                        var body1 = body.slice(7);
-                        var body2 = JSON.parse(body1);
-                        var bmr= body2['bmr'];
-                        pool.query(`INSERT INTO users (email,username,password,weight,height,age,gender,bmr) VALUES (?,?,?,?,?,?,?,${bmr})`, tempValues, function(err,result){
-                            if (!err) {
-                                res.send('User has successfully created');
-                            }else{
-                                // res.send('Something went wrong');
-                                // to check what shit 'something' goes wrong 
-                                res.send(err.message);
-                            }
-                        })
-                    }
-                });
-            }
-        })
-    }catch(err){
-        res.status(400).send(err);
+        const savedUser = await user.save();
+        res.send('User has successfully created');
+    } catch(err){
+        res.send(err.message);
     }
+
+    //Find BMI
+    var bmiAPI = {
+        method: 'GET',
+        url: `https://urvipaithankar.herokuapp.com/bmi/index.php/${height}/${weight}/${age}/${gender} 
+    })}`
+    };
+    request(bmiAPI, function (error, response, body) {
+        if (error)
+            throw new Error(error);
+        else {
+            var body1 = body.slice(7);
+            var body2 = JSON.parse(body1);
+            var bmi = body2['bmi'];
+            User.updateOne({ _id: user._id }, { $set: { bmi: `${bmi}` } }, () => {});
+        }
+    })
+    //Find BMR
+    var bmrAPI = {
+        method: 'GET',
+        url: `https://urvipaithankar.herokuapp.com/bmr/index.php/${height}/${weight}/${age}/${gender} 
+    })}`
+    };
+    request(bmrAPI, function (error, response, body) {
+        if (error)
+            throw new Error(error);
+        else {
+            var body1 = body.slice(7);
+            var body2 = JSON.parse(body1);
+            var bmr = body2['bmr'];
+            User.updateOne({ _id: user._id }, { $set: { bmr: `${bmr}` } }, () => {});
+        }
+    })
 });
 
 
 
-router.post('/login', (req,res) => {
+router.post('/login', async (req, res) => {
+    
     // VALIDATE THE DATA BEFORE MAKE A USER
     const {error} = loginValidation(req.body);
     if (error) return res.status(400).send(error.details[0].message);
 
-    var tempValues = [req.body.username, req.body.password]
-    try {
-        pool.query(`SELECT * FROM users WHERE username=? and password=?`, tempValues, function(err,result){
-            if (isEmptyObject(result)) return res.send('Username or password is wrong. Please try again.');
+    //CHECKING IF USER ALREADY EXISTS
+    const user = await User.findOne({username: req.body.username})
+    if (!user) return res.status(400).send("Username doesn't exists");
+ 
+    //PASSWORD IS CORRECT
+    const validPass = await bcrypt.compare(req.body.password, user.password);
+    if(!validPass) return res.status(400).send("Invalid password");
 
-            //CREATE AND ASSIGN A TOKEN
-            const token = jwt.sign({username: result[0].username},process.env.TOKEN_SECRET , { expiresIn: '24h' });
-            res.header('auth-token', token).send('Logged in!');
-        })        
-    }catch(err){
-        res.status(400).send(err);
-    }
+    const token = jwt.sign({_id: user._id},process.env.TOKEN_SECRET , { expiresIn: '24h' });
+    res.header('auth-token', token).send('Logged in!');
+
+    
 });
 
 router.get('/getUser', verify , (req,res) => {
     const token = req.header('Authorization');
     var decoded = jwt.decode(token, {complete: true});
-    res.send(decoded.payload.username)
+    res.send(decoded.payload._id)
 });
 
 module.exports = router;
-
-//Function to check if json file is empty
-function isEmptyObject(obj) {
-    return !Object.keys(obj).length;
-};
---
+++ routes/authmysql.js
@@ -0,0 +1,100 @@
+const router = require('express').Router();
+const mysql = require('mysql');
+require('dotenv').config();
+const jwt = require('jsonwebtoken');
+const request = require("request");
+const {registerValidation,loginValidation} = require('../validation');
+const verify = require('./verifyToken');
+
+var pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+});
+
+
+router.post('/register', (req,res) => {
+    // VALIDATE THE DATA BEFORE MAKE A USER
+    const {error} = registerValidation(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    try{
+        var height = req.body.height;
+        var weight = req.body.weight;
+        var age = req.body.age;
+        var gender = req.body.gender;
+
+        // var checkValues = [ req.body.email, req.body.username]
+        var checkValues = [req.body.username]
+        var tempValues  = [ req.body.email, req.body.username, req.body.password, req.body.weight, req.body.height, req.body.age, req.body.gender]
+
+        // pool.query(`SELECT * FROM users WHERE email=? or username=?`, checkValues, function(err,result1){
+        pool.query(`SELECT * FROM users WHERE username=?`, checkValues, function(err,result1){
+            if (!isEmptyObject(result1)) return res.status(400).send("Username or email already exist, try another one"); 
+            else{
+                //First find bmr
+                var options = {
+                    method: 'GET',
+                    url: `https://urvipaithankar.herokuapp.com/bmr/index.php/${height}/${weight}/${age}/${gender} 
+                })}`
+                };
+                request(options, function (error, response, body) {
+                    if (error) throw new Error(error); 
+                    else{
+                        //and when you find it, then create the user with all the details
+                        var body1 = body.slice(7);
+                        var body2 = JSON.parse(body1);
+                        var bmr= body2['bmr'];
+                        pool.query(`INSERT INTO users (email,username,password,weight,height,age,gender,bmr) VALUES (?,?,?,?,?,?,?,${bmr})`, tempValues, function(err,result){
+                            if (!err) {
+                                res.send('User has successfully created');
+                            }else{
+                                // res.send('Something went wrong');
+                                // to check what shit 'something' goes wrong 
+                                res.send(err.message);
+                            }
+                        })
+                    }
+                });
+            }
+        })
+    }catch(err){
+        res.status(400).send(err);
+    }
+});
+
+
+
+router.post('/login', (req,res) => {
+    // VALIDATE THE DATA BEFORE MAKE A USER
+    const {error} = loginValidation(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    var tempValues = [req.body.username, req.body.password]
+    try {
+        pool.query(`SELECT * FROM users WHERE username=? and password=?`, tempValues, function(err,result){
+            if (isEmptyObject(result)) return res.send('Username or password is wrong. Please try again.');
+
+            //CREATE AND ASSIGN A TOKEN
+            const token = jwt.sign({username: result[0].username},process.env.TOKEN_SECRET , { expiresIn: '24h' });
+            res.header('auth-token', token).send('Logged in!');
+        })        
+    }catch(err){
+        res.status(400).send(err);
+    }
+});
+
+router.get('/getUser', verify , (req,res) => {
+    const token = req.header('Authorization');
+    var decoded = jwt.decode(token, {complete: true});
+    res.send(decoded.payload.username)
+});
+
+module.exports = router;
+
+//Function to check if json file is empty
+function isEmptyObject(obj) {
+    return !Object.keys(obj).length;
+};
