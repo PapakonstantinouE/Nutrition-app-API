94745dda8d8d5bb25d64f525af6d465b39f011f6
olyxen <dai16058@uom.edu.gr>
2020-07-12 03:44:58 +0300

    Create a service to copy meals

--- routes/meals.js
+++ routes/meals.js
@@ -6,7 +6,6 @@ const verify = require('./verifyToken');
 
 
 router.get('/getDailyStats/:date', verify, (req,res,next) => {
-    console.log(req.body)
     var user = req.user._id;
     var dateReq = req.params.date;
 
@@ -14,7 +13,7 @@ router.get('/getDailyStats/:date', verify, (req,res,next) => {
     //thn arxh kai to telos ths hmeras pou thelw na psa3w gia geumata
     var b = dateReq.split(/\D+/);
     var startDate = new Date(Date.UTC(b[0], --b[1], b[2]));
-    var endDate =  new Date(Date.UTC(b[0], b[1], ++b[2]));
+    var endDate =  new Date(Date.UTC(b[0], b[1], b[2],23,59,59));
     Meals.find({user_id: user, date: {$gte: startDate, $lt: endDate}})
     .then((meals) => {
         var totalPr = 0;
@@ -62,7 +61,6 @@ router.get('/getDailyStats/:date', verify, (req,res,next) => {
 
 
 router.get('/getDailyCalories/:date', verify, (req,res,next) => {
-    console.log(req.body)
     var user = req.user._id;
     var dateReq = req.params.date;
 
@@ -70,7 +68,7 @@ router.get('/getDailyCalories/:date', verify, (req,res,next) => {
     //thn arxh kai to telos ths hmeras pou thelw na psa3w gia geumata
     var b = dateReq.split(/\D+/);
     var startDate = new Date(Date.UTC(b[0], --b[1], b[2]));
-    var endDate =  new Date(Date.UTC(b[0], b[1], ++b[2]));
+    var endDate =  new Date(Date.UTC(b[0], b[1], b[2],23,59,59));
 
     Meals.find({user_id: user, date: {$gte: startDate, $lt: endDate}})
   
@@ -103,11 +101,10 @@ router.get('/getWeeklyCalories/:date', verify, async (req,res,next) => {
     var b = dateReq.split(/\D+/);
 
     var tableCal = [];
-    var i = 6;
-    for(i;i>=0;i--){
+    for(i=6;i>=0;i--){
         
         var startDate = new Date(Date.UTC(b[0], b[1]-1, b[2]-i))
-        var endDate =  new Date(Date.UTC(b[0], b[1]-1, b[2]-(i-1)));
+        var endDate =  new Date(Date.UTC(b[0], b[1]-1, b[2]-i,23,59,59));
         
         const meals = await Meals.find({user_id: user, date: {$gte: startDate, $lt: endDate}})
         
@@ -145,7 +142,7 @@ router.get('/getWeeklynutri/:date/:nutri', verify, async (req,res,next) => {
     for(i;i>=0;i--){
         
         var startDate = new Date(Date.UTC(b[0], b[1]-1, b[2]-i))
-        var endDate =  new Date(Date.UTC(b[0], b[1]-1, b[2]-(i-1)));
+        var endDate =  new Date(Date.UTC(b[0], b[1]-1, b[2]-i,23,59,59));
         
         const meals = await Meals.find({user_id: user, date: {$gte: startDate, $lt: endDate}})
         
@@ -184,7 +181,7 @@ router.put('/addMeal', verify, (req,res,next) => {
     //thn arxh kai to telos ths hmeras pou thelw na psa3w gia geumata
     var b = dateReq.split(/\D+/);
     var startDate = new Date(Date.UTC(b[0], --b[1], b[2]));
-    var endDate =  new Date(Date.UTC(b[0], b[1], ++b[2]));
+    var endDate =  new Date(Date.UTC(b[0], b[1], b[2],23,59,59));
 
 
     Meals.findOne({user_id: userReq, mealkind: mealReq, date: {$gte: startDate, $lt: endDate}})
@@ -195,6 +192,7 @@ router.put('/addMeal', verify, (req,res,next) => {
             meal.calories += Number(req.body.ingredients[0].calories) 
             meal.save()
             .then((meal) => {
+                console.log('Food added ', meal);
                 res.statusCode = 200;
                 res.setHeader('Content-Type', 'application/json');
                 res.json(meal);                
@@ -224,7 +222,7 @@ router.get('/getMeal/:date', verify, (req,res,next) => {
     //thn arxh kai to telos ths hmeras pou thelw na psa3w gia geumata
     var b = dateReq.split(/\D+/);
     var startDate = new Date(Date.UTC(b[0], --b[1], b[2]));
-    var endDate =  new Date(Date.UTC(b[0], b[1], ++b[2]));
+    var endDate =  new Date(Date.UTC(b[0], b[1], b[2],23,59,59));
 
     Meals.find({user_id: userReq, date: {$gte: startDate, $lt: endDate}})
     .then((meals) => {
@@ -244,10 +242,10 @@ router.delete('/deleteMeal/:date/:meal/:food_id', verify, (req,res,next) => {
 
     var b = dateReq.split(/\D+/);
     var startDate = new Date(Date.UTC(b[0], --b[1], b[2]));
-    var endDate =  new Date(Date.UTC(b[0], b[1], ++b[2]));
+    var endDate =  new Date(Date.UTC(b[0], b[1], b[2],23,59,59));
 
     Meals.updateOne( {user_id: userReq, mealkind: mealReq, date: {$gte: startDate, $lt: endDate}},
-        {$pull:{ingredients:{fatSecret_id: food_id}}},
+        {$pull:{ingredients:{_id: food_id}}},
         { new: true })
     .then(() => {
         Meals.findOne({user_id: userReq, mealkind: mealReq, date: {$gte: startDate, $lt: endDate}})
@@ -260,8 +258,6 @@ router.delete('/deleteMeal/:date/:meal/:food_id', verify, (req,res,next) => {
             meal.calories = totalCal;
             meal.save()
             .then((meal) => {
-                console.log(totalCal)
-                console.log(meal)
                 res.statusCode = 200;
                 res.setHeader('Content-Type', 'application/json');
                 res.json(meal);                
@@ -280,7 +276,7 @@ router.get('/getMealsNutri/:date', verify, (req,res,next) => {
 
     var b = dateReq.split(/\D+/);
     var startDate = new Date(Date.UTC(b[0], --b[1], b[2]));
-    var endDate =  new Date(Date.UTC(b[0], b[1], ++b[2]));
+    var endDate =  new Date(Date.UTC(b[0], b[1], b[2],23,59,59));
 
     Meals.find({user_id: user, date: {$gte: startDate, $lt: endDate}})
     .then((meals) => {
@@ -331,6 +327,64 @@ router.get('/getMealsNutri/:date', verify, (req,res,next) => {
         res.json(nutrients)
     }, (err) => next(err))
     .catch((err) => next(err));
+})
+
+router.post('/copyMeal/:originDate/:mealKind/:copyDate', verify, async (req,res,next) => {
+    var user = req.user._id;
+    var mealReq = req.params.mealKind
+    var originDate = req.params.originDate;
+    var copyDate = req.params.copyDate;
+  
+    var b = originDate.split(/\D+/);
+    var startDate = new Date(Date.UTC(b[0], --b[1], b[2]));
+    var endDate =  new Date(Date.UTC(b[0], b[1], b[2],23,59,59));
+
+    var b = copyDate.split(/\D+/);
+    var startDate2 = new Date(Date.UTC(b[0], --b[1], b[2]));
+    var endDate2 =  new Date(Date.UTC(b[0], b[1], b[2],23,59,59));
+
+
+    Meals.find({user_id: user, mealkind: mealReq, date: {$gte: startDate, $lt: endDate}})
+    .then((meals) => {
+        var newDoc = meals[0];
+        Meals.findOne({user_id: user, mealkind: mealReq, date: {$gte: startDate2, $lt: endDate2}})
+        .then((meal) => {
+            //an uparxei hdh eggrafh gia auto to geuma apla to enhmerwnei
+            if (meal != null) {
+                for(i=0; i<newDoc.ingredients.length; i++){
+                    newDoc.ingredients[i]._id = new mongoose.Types.ObjectId();
+                    meal.ingredients.push(newDoc.ingredients[i]);
+                }
+                meal.calories += Number(newDoc.calories) 
+                meal.save()
+                .then((meal) => {
+                    console.log('Meal Copy added:', meal);
+                    res.statusCode = 200;
+                    res.setHeader('Content-Type', 'application/json');
+                    res.json(meal);                
+                }, (err) => next(err));
+            }
+            else {
+                //allazw thn hmeromhnia me authn p thelw na prostethei to geuma
+                newDoc.date = copyDate
+                // dimiourgw kainourio id gia to geuma gt den mporoume na exoume 2 eggrafes me idio id
+                newDoc._id = new mongoose.Types.ObjectId()
+                //allazei ta id tou kathe fagitou sto geuma auto
+                for(i=0;i<newDoc.ingredients.length;i++){
+                    newDoc.ingredients[i]._id = new mongoose.Types.ObjectId();
+                }
+                Meals.insertMany(newDoc)
+                .then((newDocs) => {
+                    console.log('Meal Copied', newDocs);
+                    res.statusCode = 200;
+                    res.setHeader('Content-Type', 'application/json');
+                    res.json(newDocs);
+                })
+            }    
+        }, (err) => next(err))
+    .catch((err) => next(err));
+    })
+    
 })
 
 module.exports = router;
